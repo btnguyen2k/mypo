ce78966b78a0a5b7959144f8cbb317c530cb8f13
Thanh Nguyen <btnguyen2k@gmail.com>
2026-01-06 15:08:45 +1100

    Refactor sidebar menu

--- MyPo.Blazor/MyPo.Blazor.Portfolio.App/Bootstrap/MenuBootstrapper.cs
+++ MyPo.Blazor/MyPo.Blazor.Portfolio.App/Bootstrap/MenuBootstrapper.cs
@@ -11,18 +11,26 @@ public class MenuBootstrapper
 {
 	public static void ConfigureServices(IServiceCollection _)
 	{
-		Sidebar.AddOrReplaceSection(new Sidebar.SidebarSection
+		Sidebar.AddOrReplaceEntry(new Sidebar.SidebarItem
+		{
+			Id = "markets",
+			Label = "Markets",
+			Icon = "bi-bank",
+			Url = PortfolioUIGlobals.ROUTE_PORTFOLIO_MARKETS,
+		});
+
+		Sidebar.AddOrReplaceEntry(new Sidebar.SidebarSection
 		{
 			Id = "portfolio",
 			Label = "Portfolio",
 			Items = [
-				new Sidebar.SidebarItem
-				{
-					Id = "markets",
-					Label = "Markets",
-					Icon = "bi-bank",
-					Url = PortfolioUIGlobals.ROUTE_PORTFOLIO_MARKETS,
-				}
+				// new Sidebar.SidebarItem
+				// {
+				// 	Id = "markets",
+				// 	Label = "Markets",
+				// 	Icon = "bi-bank",
+				// 	Url = PortfolioUIGlobals.ROUTE_PORTFOLIO_MARKETS,
+				// }
 			],
 		});
 	}
