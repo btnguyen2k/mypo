58d8ba28698bea830ac033a9d7a442725d14ae27
Thanh Nguyen <btnguyen2k@gmail.com>
2026-01-21 17:14:11 +1100

    Refactor CAlert

--- MyPo.Blazor/MyPo.Blazor.App/Shared/BasePage.razor.cs
+++ MyPo.Blazor/MyPo.Blazor.App/Shared/BasePage.razor.cs
@@ -9,25 +9,49 @@ public abstract class BasePage : BaseComponent
 {
 	protected string AlertMessage { get; set; } = string.Empty;
 	protected string AlertType { get; set; } = "info";
-	protected bool AlertHasChanged {get; set; } = false;
+	// protected bool AlertHasChanged {get; set; } = false;
+	// protected DateTimeOffset? AlertAutoCloseTimestamp { get; set; } = null;
 	protected bool HideUI { get; set; } = false;
+
+	protected CAlert? Alert { get; set; } = null;
 
 	protected void CloseAlert()
 	{
-		AlertMessage = string.Empty;
-		AlertHasChanged = false;
+		if (Alert == null) return;
+		Alert.Hide();
+		// AlertMessage = string.Empty;
+		// AlertHasChanged = false;
+		// AlertAutoCloseTimestamp = null;
 		StateHasChanged();
 	}
 
-	protected void ShowAlert(string type, string message)
+	protected void ShowAlert(string type, string message, int autoCloseAfterMs = 0)
 	{
-		var oldAlertType = AlertType;
-		var oldAlertMessage = AlertMessage;
-		AlertType = type;
-		AlertMessage = message;
-		AlertHasChanged = !String.IsNullOrEmpty(oldAlertMessage)
-			&& (String.Compare(oldAlertMessage, message, MyPo.Shared.Globals.StringComparison) != 0
-				|| String.Compare(oldAlertType, type, MyPo.Shared.Globals.StringComparison) != 0);
+		if (Alert == null) return;
+		Alert.Show(type, message, autoCloseAfterMs);
+		// var oldAlertType = AlertType;
+		// var oldAlertMessage = AlertMessage;
+		// AlertType = type;
+		// AlertMessage = message;
+		// if (autoCloseAfterMs > 0)
+		// {
+		// 	AlertAutoCloseTimestamp = DateTimeOffset.Now.AddMilliseconds(autoCloseAfterMs);
+		// 	Task.Run(async () =>
+		// 	{
+		// 		await Task.Delay(autoCloseAfterMs);
+		// 		if (AlertAutoCloseTimestamp != null && AlertAutoCloseTimestamp.Value <= DateTimeOffset.Now)
+		// 		{
+		// 			CloseAlert();
+		// 		}
+		// 	});
+		// }
+		// else
+		// {
+		// 	AlertAutoCloseTimestamp = null;
+		// }
+		// AlertHasChanged = !String.IsNullOrEmpty(oldAlertMessage)
+		// 	&& (String.Compare(oldAlertMessage, message, MyPo.Shared.Globals.StringComparison) != 0
+		// 		|| String.Compare(oldAlertType, type, MyPo.Shared.Globals.StringComparison) != 0);
 		StateHasChanged();
 	}
 
