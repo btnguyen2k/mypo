70dcdbbcaef9e623317638272195e28e3ed3516a
Thanh Nguyen <btnguyen2k@gmail.com>
2026-01-24 23:40:21 +1100

    Refactor PortfolioDbContextRepository

---
+++ MyPo.Portfolio.Shared/EF/PortfolioDbContextRepository.PortfolioRoi.cs
@@ -0,0 +1,75 @@
+﻿using MyPo.Portfolio.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyPo.Portfolio.Shared.EF;
+
+public sealed partial class PortfolioDbContextRepository
+{
+	private DbSet<RoiRec> RoiRecStore { get; set; }
+
+	public async ValueTask<RoiRec?> CreateRoiRecAsync(RoiRec roiRec, CancellationToken cancellationToken = default)
+	{
+		var entry = await RoiRecStore.AddAsync(roiRec, cancellationToken);
+		return await SaveChangesAsync(cancellationToken) > 0 ? entry.Entity : null;
+	}
+
+	/// <inheritdoc />
+	public async ValueTask<PnlSummary> GetRoiSummaryForPortfolioAsync(string portfolioId, CancellationToken cancellationToken = default)
+	{
+		var roiSummary = new PnlSummary()
+		{
+			PortfolioId = portfolioId,
+			TotalBuyValue = 0.0m,
+			TotalSellValue = 0.0m,
+			TotalDividends = 0.0m,
+			TotalFees = 0.0m,
+			TotalCashIn = 0.0m,
+			TotalCashOut = 0.0m,
+		};
+		var rows = await RoiRecStore.AsNoTracking()
+			.Where(rr => rr.PortfolioId == portfolioId)
+			.Where(rr => rr.Status != RoiRec.STATUS_ARCHIVED)
+			.GroupBy(rr => rr.TxType)
+			.Select(g => new
+			{
+				TxType = g.Key,
+				TotalValue = g.Sum(rr => rr.TxValue)
+			})
+			.ToListAsync(cancellationToken);
+		foreach (var row in rows)
+		{
+			switch (row.TxType)
+			{
+				case RoiRec.TX_TYPE_BUY:
+					roiSummary.TotalBuyValue = row.TotalValue;
+					break;
+				case RoiRec.TX_TYPE_SELL:
+					roiSummary.TotalSellValue = row.TotalValue;
+					break;
+				case RoiRec.TX_TYPE_DIVIDEND:
+					roiSummary.TotalDividends = row.TotalValue;
+					break;
+				case RoiRec.TX_TYPE_FEE:
+					roiSummary.TotalFees = row.TotalValue;
+					break;
+				case RoiRec.TX_TYPE_CASHIN:
+					roiSummary.TotalCashIn = row.TotalValue;
+					break;
+				case RoiRec.TX_TYPE_CASHOUT:
+					roiSummary.TotalCashOut = row.TotalValue;
+					break;
+			}
+		}
+
+		return roiSummary;
+	}
+
+	/// <inheritdoc />
+	public async ValueTask<IEnumerable<RoiRec>> GetRoiRecsByPortfolioIdAsync(string portfolioId, CancellationToken cancellationToken = default)
+	{
+		return await RoiRecStore.AsNoTracking()
+			.Where(rr => rr.PortfolioId == portfolioId).Where(rr => rr.Status != RoiRec.STATUS_ARCHIVED)
+			.OrderByDescending(rr => rr.TxTime)
+			.ToListAsync(cancellationToken);
+	}
+}
