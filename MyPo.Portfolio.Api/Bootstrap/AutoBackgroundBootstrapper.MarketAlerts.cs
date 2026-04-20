ee20cdd5ba72e4da71a7250d9cca00b197b62f06
Thanh Nguyen <btnguyen2k@gmail.com>
2026-04-20 10:14:31 +1000

    Resolve CodeQL warnings

--- MyPo.Portfolio.Api/Bootstrap/AutoBackgroundBootstrapper.MarketAlerts.cs
+++ MyPo.Portfolio.Api/Bootstrap/AutoBackgroundBootstrapper.MarketAlerts.cs
@@ -185,41 +185,41 @@ sealed class AutoBackgroundSendMarketAlerts : AutoBackgroundAnnouncementScanner
 		var quotesMap = await TickerUtils.FetchQuotesForTickersAsync(
 			eventsListing.Select(e => e.ItemCode).Distinct(),
 			finHubClient, cancellationToken: cancellationToken);
-		var message = "<strong>🆕 New listings:</strong>\n<blockquote>";
+		var message = new StringBuilder("<strong>🆕 New listings:</strong>\n<blockquote>");
 		foreach (var e in eventsListing)
 		{
 			var tz = MarketEventUtils.MarketToDefaultTimeZoneId(e.MarketId);
 			var ticker = YFUtils.BuildYFTicker(e.ItemCode);
 			var quoteInfo = quotesMap.TryGetValue(ticker, out var quote) ? $"(curr: <code>{quote.MarketPrice:F2}</code>)" : "";
-			message += $"<a href=\"{e.Metadata?.Link}\">{e.ItemCode}</a> - 📅 <code>{e.EventTime.ToTimeZoneSilently(tz):MMM-dd}</code> -💲<code>{e.Metadata?.Listing?.Price??0:F2}</code> {quoteInfo}\n";
+			message.Append($"<a href=\"{e.Metadata?.Link}\">{e.ItemCode}</a> - 📅 <code>{e.EventTime.ToTimeZoneSilently(tz):MMM-dd}</code> -💲<code>{e.Metadata?.Listing?.Price??0:F2}</code> {quoteInfo}\n");
 			if (e.Metadata?.Listing?.Analysis?.Outlook != null)
 			{
 				if (e.Metadata.Listing.Analysis.Outlook.TryGetValue("w2", out var v21))
 				{
-					message += $"📈 2w: {v21.Direction} ({v21.Confidence}%), {v21.Reason}\n";
+					message.Append($"📈 2w: {v21.Direction} ({v21.Confidence}%), {v21.Reason}\n");
 				}
 				if (e.Metadata.Listing.Analysis.Outlook.TryGetValue("m1", out var m11))
 				{
-					message += $"📈 1m: {m11.Direction} ({m11.Confidence}%), {m11.Reason}\n";
+					message.Append($"📈 1m: {m11.Direction} ({m11.Confidence}%), {m11.Reason}\n");
 				}
 				if (e.Metadata.Listing.Analysis.Outlook.TryGetValue("m3", out var m31))
 				{
-					message += $"📈 3m: {m31.Direction} ({m31.Confidence}%), {m31.Reason}\n";
+					message.Append($"📈 3m: {m31.Direction} ({m31.Confidence}%), {m31.Reason}\n");
 				}
 			}
-			message += "\n";
+			message.Append('\n');
 		}
-		message += "</blockquote><preview disabled />";
-		// message += "</blockquote>";
+		message.Append("</blockquote><preview disabled />");
+		// message.Append("</blockquote>");
 
 		foreach (var chatId in chatIDs)
 		try
 		{
-			await teleBot.SendHtml(chatId, message);
+			await teleBot.SendHtml(chatId, message.ToString());
 		}
 		catch (Exception ex)
 		{
-			Logger.LogError(ex, "Failed to send market alert for chat ID {chatId}: {message}", chatId, message);
+			Logger.LogError(ex, "Failed to send market alert for chat ID {chatId}: {message}", chatId, message.ToString());
 		}
 	}
 }
