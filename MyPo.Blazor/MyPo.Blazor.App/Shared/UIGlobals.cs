2d282241b486ea188e327c3a7338ab2bd6209168
Thanh Nguyen <btnguyen2k@gmail.com>
2026-05-08 03:54:25 +1000

    (Deps) Migrate to CoreUI-Free-Bootrap v5.5.0

--- MyPo.Blazor/MyPo.Blazor.App/Shared/UIGlobals.cs
+++ MyPo.Blazor/MyPo.Blazor.App/Shared/UIGlobals.cs
@@ -4,7 +4,8 @@ public class UIGlobals
 {
 	private static readonly string PackageId = typeof(UIGlobals).Assembly.GetName().Name!;
 	public static readonly string ASSET_ROOT = $"/_content/{PackageId}";
-	public static readonly string COREUI_BASE = $"{ASSET_ROOT}/coreui-free-bootstrap-v5.1.0";
+	// public static readonly string COREUI_BASE = $"{ASSET_ROOT}/coreui-free-bootstrap-v5.1.0";
+	public static readonly string COREUI_BASE = $"{ASSET_ROOT}/coreui-free-bootstrap-v5.5.0";
 	public static readonly string BOOTSTRAP_ICONS_BASE = $"{ASSET_ROOT}/bootstrap-icons-1.11.3";
 
 	public const string ROUTE_LANDING = "/";
