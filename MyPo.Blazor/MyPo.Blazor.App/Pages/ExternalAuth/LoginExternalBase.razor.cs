d7eb2120d324afd7de7588e582088309a1db937d
Thanh Nguyen <btnguyen2k@gmail.com>
2026-05-08 14:58:44 +1000

    Migrate to CoreUI-Free-Bootrap v5.5.0: UI rework

--- MyPo.Blazor/MyPo.Blazor.App/Pages/ExternalAuth/LoginExternalBase.razor.cs
+++ MyPo.Blazor/MyPo.Blazor.App/Pages/ExternalAuth/LoginExternalBase.razor.cs
@@ -1,11 +1,29 @@
-using System.Net;
+﻿using System.Net;
 using MyPo.Blazor.App.Shared;
 using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace MyPo.Blazor.App.Pages.ExternalAuth;
 
 public abstract class LoginExternalBase : BaseComponent
 {
+	protected string AlertType { get; set; } = string.Empty;
+	protected string AlertTitle { get; set; } = string.Empty;
+	protected string AlertMessage { get; set; } = string.Empty;
+	protected bool ShowReturnLinks { get; set; } = false;
+
+	[Inject]
+	protected AuthenticationStateProvider AuthenticationStateProvider { get; set; } = default!;
+
+	protected void ShowAlert(string type, string message, string title = "")
+	{
+		AlertType = type;
+		AlertTitle = title;
+		AlertMessage = message;
+		StateHasChanged();
+	}
+
 	protected IDictionary<string, string> ParseQueryParams(bool urlDecode = true, bool htmlDecode = false)
 	{
 		var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
