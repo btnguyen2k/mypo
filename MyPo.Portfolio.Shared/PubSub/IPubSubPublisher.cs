09663e00460dca7a43f87d1e5927c6ddb505f2be
Thanh Nguyen <btnguyen2k@gmail.com>
2026-09-16 11:27:46 +1000

    chore(docs)

--- MyPo.Portfolio.Shared/PubSub/IPubSubPublisher.cs
+++ MyPo.Portfolio.Shared/PubSub/IPubSubPublisher.cs
@@ -5,6 +5,13 @@
 /// </summary>
 public interface IPubSubPublisher
 {
+    /// <summary>
+    /// Publishes the specified application message asynchronously.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of the message to publish.</typeparam>
+    /// <param name="message">The message to publish.</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     ValueTask PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken = default)
         where TMessage : class, IPubSubMessage;
 }
