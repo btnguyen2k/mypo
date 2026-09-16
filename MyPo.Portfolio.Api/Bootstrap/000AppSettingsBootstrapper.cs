83ef2991c0979d4d81b33819b7f09382f346e109
Thanh Nguyen <btnguyen2k@gmail.com>
2026-09-16 14:02:53 +1000

    refactor(appsettings): Move Portfolio component config to a separated appsettings.json file

---
+++ MyPo.Portfolio.Api/Bootstrap/000AppSettingsBootstrapper.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using MyPo.Shared.Bootstrap;
+
+namespace MyPo.Portfolio.Api.Bootstrap;
+
+/// <summary>
+/// Bootstrapper that loads additional application settings from the embedded .json file.
+/// </summary>
+/// <remarks>
+/// This bootstrap needs to be executed as early as possible to ensure that the application settings are loaded before
+/// other bootstrappers that depend on them.
+/// </remarks>
+[Bootstrapper(Priority = 0)]
+public class AppSettingsBootstrapper
+{
+    private const string APP_SETTINGS_FILE = "Resources.appsettings.json";
+    private static readonly ILogger<AppSettingsBootstrapper> logger
+        = LoggerFactory.Create(b => b.AddConsole()).CreateLogger<AppSettingsBootstrapper>();
+
+    public static void ConfigureBuilder(WebApplicationBuilder appBuilder)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var resourceName = $"{assembly.GetName().Name}.{APP_SETTINGS_FILE}";
+        logger.LogInformation("Loading '{}'...", resourceName);
+        var availableResources = assembly.GetManifestResourceNames();
+        if (Array.IndexOf(availableResources, resourceName) == -1)
+        {
+            throw new FileNotFoundException($"'{resourceName}' not found in assembly resources.");
+        }
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        var myAppSettings = new ConfigurationBuilder()
+            .AddJsonStream(stream!)
+            .AddEnvironmentVariables()
+            .Build();
+        appBuilder.Configuration.AddConfiguration(myAppSettings);
+    }
+}
--- MyPo.Portfolio.Api/Bootstrap/ExternalServicesBootstrapper.cs
+++ MyPo.Portfolio.Api/Bootstrap/ExternalServicesBootstrapper.cs
@@ -1,40 +1,20 @@
 ﻿using Finhub.Client;
 using MyPo.Portfolio.Api.Services;
 using MyPo.Shared.Bootstrap;
-using System.Reflection;
 
 namespace MyPo.Portfolio.Api.Bootstrap;
 
 [Bootstrapper]
 public class ExternalServicesBootstrapper
 {
-    private const string EXTERNAL_SERVICES_SETTINGS_FILE = "Resources.ext_services_settings.json";
-    private static readonly ILogger<ExternalServicesBootstrapper> logger = LoggerFactory.Create(b => b.AddConsole()).CreateLogger<ExternalServicesBootstrapper>();
+    private static readonly ILogger<ExternalServicesBootstrapper> logger
+        = LoggerFactory.Create(b => b.AddConsole()).CreateLogger<ExternalServicesBootstrapper>();
 
     public static void ConfigureBuilder(WebApplicationBuilder appBuilder)
     {
-        var assembly = Assembly.GetExecutingAssembly();
-        var resourceName = $"{assembly.GetName().Name}.{EXTERNAL_SERVICES_SETTINGS_FILE}";
-        logger.LogInformation("Loading '{}'...", resourceName);
-        var availableResources = assembly.GetManifestResourceNames();
-        if (Array.IndexOf(availableResources, resourceName) == -1)
-        {
-            throw new FileNotFoundException($"'{resourceName}' not found in assembly resources.");
-        }
-        using (var stream = assembly.GetManifestResourceStream(resourceName))
-        {
-            var externalServicesSettings = new ConfigurationBuilder()
-                .AddJsonStream(stream!)
-                .AddEnvironmentVariables()
-                .Build();
+        logger.LogInformation("Configuring FinHub API services...");
 
-            appBuilder.Configuration.AddConfiguration(externalServicesSettings);
-            ConfigureExternalServices(appBuilder, externalServicesSettings);
-        }
-    }
-
-    private static void ConfigureExternalServices(WebApplicationBuilder appBuilder, IConfiguration externalServicesSettings)
-    {
+        var externalServicesSettings = appBuilder.Configuration;
         var finhubBaseUrl = externalServicesSettings.GetValue<string>("FinHub:Url");
         if (!Uri.TryCreate(finhubBaseUrl, UriKind.Absolute, out _))
         {
