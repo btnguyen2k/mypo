e3de60c254036e3fdc3edea2c83e074651248414
Thanh Nguyen <btnguyen2k@gmail.com>
2026-09-16 10:16:21 +1000

    feat(portfolio): Cleanup post-repoting a portfolio.

---
+++ dbschema/pgsql/patch_pgsql-to-v2.4.0.sql
@@ -0,0 +1,10 @@
+﻿ALTER TABLE mypo_report
+    ADD CONSTRAINT fk_mypo_report_portfolio_id_mypo_portfolio_id
+    FOREIGN KEY (portfolio_id)
+    REFERENCES mypo_portfolio (portfolio_id)
+    ON DELETE CASCADE;
+
+CREATE INDEX idx_mypo_report_portfolio_id ON mypo_report (portfolio_id);
+
+DROP INDEX uidx_mypo_report;
+CREATE UNIQUE INDEX uidx_mypo_report ON mypo_report (portfolio_id, report_type, report_period_start, item_code, tx_type);
--- dbschema/pgsql/schema_pgsql_report.sql
+++ dbschema/pgsql/schema_pgsql_report.sql
@@ -6,7 +6,7 @@ CREATE TABLE mypo_report (
     report_id varchar(48) NOT NULL,
     report_type varchar(16) NOT NULL,           -- WEEKLY, MONTHLY, QUARTERLY, YEARLY
     report_period_start varchar(10) NOT NULL,   -- YYYY-MM-DD, mark the start date of the report period,
-    report_period varchar(16) NOT NULL,          -- Period label: FY2024-25-W01 for weekly, 2024-01 for monthly, FY2024-25-Q1 for quarterly, FY2024-25 for yearly
+    report_period varchar(16) NOT NULL,         -- Period label: FY2024-25-W01 for weekly, 2024-01 for monthly, FY2024-25-Q1 for quarterly, FY2024-25 for yearly
     portfolio_id varchar(48) NOT NULL,
     item_code varchar(16) NOT NULL,             -- in EXCHANGE:SYMBOL format
     tx_type varchar(16) NOT NULL,               -- BUY, SELL, TAX, etc
@@ -19,6 +19,8 @@ CREATE TABLE mypo_report (
     created_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp with time zone NOT NULL DEFAULT CURRENT_TIMESTAMP,
     concurrency_stamp varchar(48) NULL,
-    CONSTRAINT pk_mypo_report PRIMARY KEY (report_id)
+    CONSTRAINT pk_mypo_report PRIMARY KEY (report_id),
+    CONSTRAINT fk_mypo_report_portfolio_id_mypo_portfolio_id FOREIGN KEY (portfolio_id) REFERENCES mypo_portfolio (portfolio_id) ON DELETE CASCADE
 );
-CREATE INDEX uidx_mypo_report ON mypo_report (portfolio_id, report_type, report_period_start, item_code, tx_type);
+CREATE UNIQUE INDEX uidx_mypo_report ON mypo_report (portfolio_id, report_type, report_period_start, item_code, tx_type);
+CREATE INDEX idx_mypo_report_portfolio_id ON mypo_report (portfolio_id);
